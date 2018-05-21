99263af4f14aadf2052b7aece2381769b68abd54
0Zarathustra0 <1915744914@qq.com>
2018-05-21 15:45:04 +0800

    将每个空文件夹都建立了一个Test类，防止无法上传完整的目录结构

---
+++ Qingcheng/src/main/java/nsu/edu/qingcheng/backstage/service/impl/Test.java
@@ -0,0 +1,9 @@
+package nsu.edu.qingcheng.backstage.service.impl;
+/** 
+* @author zhangshuqiang <br>
+* @version 2018年5月21日 下午3:38:52 <br>
+* 功能：填充空文件夹的Test。为了上传完整的项目目录结构，所以不能有空的文件夹
+*/
+public class Test {
+
+}
